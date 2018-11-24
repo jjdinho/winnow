e04b6498652d41527b772fc7af077b33061fb33c
Jake Johnson <johnson.s.jake@gmail.com>
2018-11-24 12:09:32 +0100

    made first api requests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    newsapi = News.new(ENV['NEWS_API_KEY'])
+    @sources = newsapi.get_everything(q: 'bitcoin',
+                                      sources: 'bbc-news,the-verge',
+                                      domains: 'bbc.co.uk,techcrunch.com',
+                                      from: '2018-11-17',
+                                      to: '2018-11-24',
+                                      language: 'en',
+                                      sortBy: 'popularity',
+                                      pageSize: 100)
   end
 end
