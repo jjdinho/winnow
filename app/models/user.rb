927067302c8f311054037b92120b8969490c457e
Jake Johnson <johnson.s.jake@gmail.com>
2018-11-25 18:17:42 +0100

    send welcome email on user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :send_welcome_email
+
   def name
     first_name + " " + last_name
+  end
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
   end
 end
