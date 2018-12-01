f9f183b1174d2c06d28d1cfc2a3959532d3060d6
Jake Johnson <johnson.s.jake@gmail.com>
2018-12-01 15:24:32 +0100

    nest search queries in news feed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :news_feeds, only: [:index,:new,:create]
-  resources :search_queries, only: [:index,:new,:create]
+  resources :news_feeds, only: [:index,:new,:create] do
+    resources :search_queries, only: [:index,:new,:create]
+  end
 
   get 'home_search_query/:query/:sources', to: 'pages#home_search_query'
 
