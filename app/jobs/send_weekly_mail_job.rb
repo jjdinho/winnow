a29372c7816d7331422c518f4bfcb4c170051d6e
Jake Johnson <johnson.s.jake@gmail.com>
2018-12-28 15:58:45 -0600

    add

--- app/jobs/send_weekly_mail_job.rb
+++ app/jobs/send_weekly_mail_job.rb
@@ -2,12 +2,10 @@ class SendWeeklyMailJob < ApplicationJob
   queue_as :default
 
   def perform
-    counter = 0
     weekly_news_feeds = NewsFeed.where(frequency: "weekly", active: true)
     weekly_news_feeds.each do |feed|
       NewsFeedMailer.news_feed_weekly(feed).deliver_now
-      counter += 1
     end
+    puts "Sent #{weekly_news_feeds.length} emails"
   end
-  return "Sent #{counter} emails."
 end
