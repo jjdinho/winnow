bdad6d14c762b12d67510a5873c776e1b27329bd
Jake Johnson <johnson.s.jake@gmail.com>
2018-12-30 17:40:14 -0600

    set up stimulus home signupcontaoler

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,21 @@ module ApplicationHelper
       kind
     end
   end
+
+  # Devise for sign up form on home page
+  def resource_name
+    :user
+  end
+
+  def resource_class
+     User
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
