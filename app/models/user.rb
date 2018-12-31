a19313a41e7f5bf392f65460bdbbd3facec5c620
Jake Johnson <johnson.s.jake@gmail.com>
2018-12-31 12:07:19 -0600

    add user to mailchimp after creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   after_create :send_welcome_email
+  after_create :add_to_mailchimp
 
   def name
     first_name && last_name ? first_name + " " + last_name : nil
@@ -16,5 +17,15 @@ class User < ApplicationRecord
 
   def send_welcome_email
     UserMailer.welcome(self).deliver_now
+  end
+
+  def add_to_mailchimp
+    return unless Rails.env.production?
+    first_name.nil? ? first = "User" : first = first_name
+    last_name.nil? ? last = "#{id}" : last = last_name
+    Gibbon::Request.lists("9b875ac16e").members
+              .create(body: {email_address: email,
+              status: "subscribed",
+              merge_fields: {FNAME: first, LNAME: last}})
   end
 end
