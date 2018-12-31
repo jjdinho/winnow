87b16b0478cb78441c0a305dd3c2375dbafe6478
Jake Johnson <johnson.s.jake@gmail.com>
2018-12-31 12:38:06 -0600

    set up pundit and custom error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Pundit
+
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
+
+  def user_not_authorized
+    flash[:alert] = "Oops! Apparently you cannot access that page."
+    redirect_back(fallback_location: root_path)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
