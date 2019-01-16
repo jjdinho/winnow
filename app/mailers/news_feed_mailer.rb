9d44117cf50d12584e6fe62e23d2475bdd93d5f0
Jake Johnson <johnson.s.jake@gmail.com>
2019-01-16 17:51:54 -0600

    refactor perform to job instead of mailer

--- app/mailers/news_feed_mailer.rb
+++ app/mailers/news_feed_mailer.rb
@@ -1,19 +1,17 @@
 class NewsFeedMailer < ApplicationMailer
-  def news_feed_mail(news_feed)
+  def news_feed_mail(news_feed, articles)
     # get news_feed
     @news_feed = news_feed
     @frequency = @news_feed.frequency
     @user = news_feed.user
+    @articles = articles
 
     # send email
     if @frequency == "daily"
-      @articles = @news_feed.perform_daily
       mail(to: @user.email, subject: "Daily News Feed for: #{@news_feed.keyword}", track_opens: 'true')
     elsif @frequency == "weekly"
-      @articles = @news_feed.perform_weekly
       mail(to: @user.email, subject: "Weekly News Feed for: #{@news_feed.keyword}", track_opens: 'true')
     elsif @frequency == "monthly"
-      @articles = @news_feed.perform_monthly
       mail(to: @user.email, subject: "Monthly News Feed for: #{@news_feed.keyword}", track_opens: 'true')
     end
     # This will render a view in `app/views/news_feed_mailer`!
